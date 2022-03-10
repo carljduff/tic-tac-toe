9c0ff2a1a15762a304a598d682f952c96909c2cb
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-03-10 21:27:06 +0000

    update working-branch

--- js/main.js
+++ js/main.js
@@ -18,12 +18,14 @@ class Model {
         this.endGame = [];
         this.player = ['X', 'O'];
         this.round = 0;
+        this.clear = ''
     }
 
+        
     // updateBoard = (move) => {
     //     console.log({move, element: this.board[move].textContent});
     // }
-
+    
 
 }
 
@@ -31,26 +33,31 @@ class View {
     constructor() {
         this.app = this.getElement('#app')
         this.title = this.createElement('h1')
+        this.title.className = 'title';
         this.title.textContent = 'Tic-Tac-Toe'
         this.turnTitle = this.createElement('p')
-        this.turnTitle.textContent = `X's Turn!`
+        this.turnTitle.className = 'turn';
+        this.turnTitle.innerText = `X's Turn`;
         this.container = this.createElement('div')
         this.container.className = 'container'
         this.resetButton = this.createElement('button')
+        this.resetButton.className = 'btn'
         this.resetButton.textContent = 'Reset Game'
         this.app.append(this.title, this.turnTitle, this.container, this.resetButton)
         
     }
     
+  
    
-    buildBoard = (board, handleCellEvent) => {
+    buildBoard = (board, handleCellEvent, checkWin) => {
         for (let i = 0; i < 9; i++) {
             let tile = document.createElement('div');
             tile.setAttribute('data-index', i);
+            tile.setAttribute('id', 'grid')
             board.push(tile)
             board[i].className = 'grid';
             this.container.append(board[i]);
-            board[i].addEventListener('click', handleCellEvent);
+           
         }
 
     }
@@ -74,32 +81,40 @@ class Controller {
     constructor(model, view) {
         this.m = model;
         this.v = view;
-        this.v.buildBoard(this.m.board, this.handleCellEvent)
+        this.v.buildBoard(this.m.board, this.handleCellEvent, this.checkWin)
+        // this.v.resetBoard(this.m.board, this.resetGame)
         
     }
 
 
     handleCellEvent = (e) => {
         let targetIndex = e.target.dataset.index;
-        // console.log(targetIndex)
         let tilePlace = this.m.board[targetIndex];
-        // console.log(this.turnTitle)
-    // }
-    
-    // playerTurn = () => {
+        
+       
 
         if (round % 2 == 0) {
             tilePlace.innerText = player[0];
-            turnTitle.innerText = "O's TURN";
-            console.log(turnTitle)
+            this.v.turnTitle.innerText = "O's TURN";
             round++ 
+
+            
         } else {
             tilePlace.innerText = player[1];
-            turnTitle.innerText = "X's TURN";
+            this.v.turnTitle.innerText = "X's TURN";
             round++
         }
     }
+
+    
+    checkWin = () => {
+        if(this.m.board[0].innerText == 'X' && this.m.board[1].innerText == 'X' && this.m.board[2].innerText == 'X') {
+            console.log('X WINS!')
+        }
+    }
+
 }
+
 
 
 
--- js/sandbox.js
+++ js/sandbox.js
@@ -1,19 +1,21 @@
-// const create = (type) => {
-//     const element = document.createElement(type)
-//     return element
-//   }
+const create = (type) => {
+    const element = document.createElement(type)
+    return element
+  }
 
-  
+  let btn = create('button')
+  document.body.append(btn)
+  btn.innerText = 'Button'
 //-------------------------------------------------------------------TITLE------------------------------------------------------------------------------------------
-  // let title = create('div');
-  // document.body.append(title);
-  // title.innerText = "Tic-Tac-Toe"
+  let title = create('div');
+  document.body.append(title);
+  title.innerText = "Tic-Tac-Toe"
 //-------------------------------------------------------------------TITLE------------------------------------------------------------------------------------------
 
 
 //--------------------------------------------------------------------PLAYER TURN-----------------------------------------------------------------------------------
-// let player = ['X', 'O']
-// let round = 0;
+let player = ['X', 'O']
+let round = 0;
 
 // let clicked = false;
 // function click() {
@@ -23,55 +25,57 @@
 //   } 
 // }
  
-  // if(Event.type == 'click') {
+  if(Event.type == 'click') {
 
-  // }
+  }
     
     
 
-// function turn() {
-//   if(player[0].innerText == 'X') {
-//     title.innerText = 'PLAYER 1';
-//   } 
-// }
+function turn() {
+  if(player[0].innerText == 'X') {
+    title.innerText = 'PLAYER 1';
+  } 
+}
 
 
 //--------------------------------------------------------------------PLAYER TURN-----------------------------------------------------------------------------------
 
     
 // ---------------------------------------------------------------------BOARD BUILD---------------------------------------------------------------------------------
-  //   let board = [];
-  //   let container = create('div')
-  //   container.className = 'container';
-  //   // let title = document.getElementById('title');
-  //   document.body.appendChild(container);
+    let board = [];
+    let container = create('div')
+    container.className = 'container';
+    // let title = document.getElementById('title');
+    document.body.appendChild(container);
     
 
-  //   function buildBoard() {
-  //       // let clicked = false;
-  //       for (let i = 0; i < 9; i++) {
-  //         board.push(document.createElement('div'))
-  //         board[i].className = 'grid';
-  //         container.append(board[i]);
-  //         board[i].addEventListener('click', () => {
-           
-  //           if (round % 2 == 0) {
-  //             board[i].innerText = player[0];
-  //             title.innerText = "O's TURN";
-  //             round++ 
-  //           } else {
-  //             board[i].innerText = player[1];
-  //             title.innerText = "X's TURN";
-  //             round++
-  //           }
-  //         }); //giving event listener to each of them
-  //       }
-  //   }
-  
-  
-  // buildBoard();
+    function buildBoard() {
+        // let clicked = false;
+        for (let i = 0; i < 9; i++) {
+          board.push(document.createElement('div'))
+          board[i].className = 'grid';
+          container.append(board[i]);
+          // board[i].innerText = '';
+          board[i].addEventListener('click', () => {
+            // console.log(board[i])
+            // console.log(i)
+            if (round % 2 == 0) {
+              board[i].innerText = player[0];
+              title.innerText = "PLAYER 2"
+              round++ 
+            } else {
+              board[i].innerText = player[1];
+              title.innerText = 'PLAYER 1'
+              round++
+            }
+          }); //giving event listener to each of them
+        }
+    }
+  btn.addEventListener('click', reset)
+
+  buildBoard();
+  function reset() {
+    
+  }
 
 // ---------------------------------------------------------------------BOARD BUILD--------------------------------------------------------------------------------
-
-
-
